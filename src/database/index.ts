38cf97a528f3cb2c7ef14a5485058cc9535e1134
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-04 20:51:40 +0100

    Add database folder and index.ts

---
+++ src/database/index.ts
@@ -0,0 +1,16 @@
+import chalk from "chalk";
+import mongoose from "mongoose";
+import debugCreator from "debug";
+
+const debug = debugCreator("Robots:database:root");
+
+const connectDatabase = async (url: string) => {
+  try {
+    await mongoose.connect(url);
+    debug(chalk.blue("Connected to database"));
+  } catch (error: unknown) {
+    debug(chalk.red(`Error on connection`, (error as Error).message));
+  }
+};
+
+export default connectDatabase;
