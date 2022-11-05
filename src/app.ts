0dfd10e5777848f169af4fc448cd1c04f37a19cf
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-05 13:29:12 +0100

    Add cors package and import in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import morgan from "morgan";
 import robotRouters from "./server/routers/robotRouters.js";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 app.use(morgan("dev"));
