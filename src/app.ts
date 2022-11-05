a2e4fc5ec878c7e16fde786dbaf8c9588ed4ef33
Andrea Pedreño <apedrenodiez@gmail.com>
2022-11-05 21:22:00 +0100

    Include app disable

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,18 @@ import cors from "cors";
 
 const app = express();
 
-app.use(cors({ origin: true, credentials: true }));
+app.use(
+  cors({
+    origin: [
+      "https://202209-w6chwe-catalina-tarelli-andrea.netlify.app",
+      "http://localhost:4000",
+    ],
+  })
+);
 
 app.use(morgan("dev"));
 app.use(express.json());
+app.disable("x-powered-by");
 
 app.use("/robots", robotRouters);
 
