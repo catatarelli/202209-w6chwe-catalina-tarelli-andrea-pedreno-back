a2eb8f647bfc85593ac9fea2fd12f36a3d3e9f1f
Catalina Tarelli <catatarelli@hotmail.com>
2022-11-05 12:03:06 +0100

    Modify connectDataBase function

--- src/database/index.ts
+++ src/database/index.ts
@@ -6,7 +6,7 @@ const debug = debugCreator("Robots:database:root");
 
 const connectDatabase = async (url: string) => {
   try {
-    await mongoose.connect(url);
+    await mongoose.connect(url, { dbName: "robots" });
     debug(chalk.blue("Connected to database"));
   } catch (error: unknown) {
     debug(chalk.red(`Error on connection`, (error as Error).message));
